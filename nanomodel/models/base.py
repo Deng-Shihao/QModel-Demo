33ae5d4d226aa5e13ee6b87926ab0b89f1a29d07
Shihao <sd24191@essex.ac.uk>
2025-10-19 14:48:31 +0100

    add rotation feature

--- nanomodel/models/base.py
+++ nanomodel/models/base.py
@@ -40,7 +40,7 @@ from ..nn_modules.qlinear.lookahead import configure_default_lookahead
 from ..nn_modules.qlinear.torch import TorchQuantLinear
 from ..quantization import QuantizeConfig
 from ..quantization.config import FORMAT, METHOD, QUANTIZE_BLACK_LIST, dynamic_get
-# from ..quantization.rotation.rotation import fuse_layer_norms, rotate_model FFF
+from ..quantization.rotation.rotation import fuse_layer_norms, rotate_model
 from ..utils.backend import BACKEND
 from ..utils.data import collate_data
 from ..utils.device import get_device
@@ -862,15 +862,15 @@ class BaseNanoModel(nn.Module):
             }
 
             # TODO: FFF
-            # self.model = fuse_layer_norms(model=self.model,
-            #                               pre_lm_head_norm_module_name=self.pre_lm_head_norm_module,
-            #                               **module_name_args)
+            self.model = fuse_layer_norms(model=self.model,
+                                          pre_lm_head_norm_module_name=self.pre_lm_head_norm_module,
+                                          **module_name_args)
 
             # TODO: FFF
             # MPS does not support float64.
-            # rotation_device = self.quantize_config.device if self.quantize_config.device != DEVICE.MPS else DEVICE.CPU
-            # self.model, _ = rotate_model(model=self.model, rotate_mode=self.quantize_config.rotation,
-            #                                 device=rotation_device, **module_name_args)
+            rotation_device = self.quantize_config.device if self.quantize_config.device != DEVICE.MPS else DEVICE.CPU
+            self.model, _ = rotate_model(model=self.model, rotate_mode=self.quantize_config.rotation,
+                                            device=rotation_device, **module_name_args)
 
         # init processor with default GPTQ processor
         from ..looper.tensorparallel_weight_processor import TensorParallelWeightProcessor
