74abadd2075be97156898210987ccf004951e604
Shihao <sd24191@essex.ac.uk>
2025-10-23 04:45:39 +0100

    remove

--- nanomodel/__init__.py
+++ nanomodel/__init__.py
@@ -33,11 +33,3 @@ from .quantization import BaseQuantizeConfig, QuantizeConfig
 from .utils import BACKEND
 from .version import __version__
 
-
-if os.getenv('NANOMODEL_USE_MODELSCOPE', 'False').lower() in ['true', '1']:
-    try:
-        from modelscope.utils.hf_util.patcher import patch_hub
-        patch_hub()
-    except Exception:
-        raise ModuleNotFoundError("you have set NANOMODEL_USE_MODELSCOPE env, but doesn't have modelscope? install it with `pip install modelscope`")
-
