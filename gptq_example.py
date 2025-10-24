17256d71e7b7c5f35d3069266d87d185a2aa2a01
Shihao <sd24191@essex.ac.uk>
2025-10-24 22:45:41 +0100

    fix KERNEL

--- gptq_example.py
+++ gptq_example.py
@@ -26,7 +26,7 @@ def main():
         bits=4,
         group_size=128,
         quant_method=METHOD.GPTQ,  # switch to METHOD.AWQ or METHOD.GPTQ as needed
-        kernel=KERNEL.MARLIN,        # FORMAT.MARLIN / FORMAT.GEMM / FORMAT.GEMV also available
+        kernel=KERNEL.GPTQ,        # FORMAT.MARLIN / FORMAT.GEMM / FORMAT.GEMV also available
     )
 
     logger.info("Loading pretrained model for quantization...")
