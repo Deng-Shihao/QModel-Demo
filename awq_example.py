285ff7f0ff3b4db7d6163cce0fc74657f803e8d0
Shihao <sd24191@essex.ac.uk>
2025-10-25 21:39:40 +0100

    awq_example marlin kernel

--- awq_example.py
+++ awq_example.py
@@ -23,7 +23,7 @@ def main():
         bits=4,
         group_size=128,
         quant_method=METHOD.AWQ,  # switch to METHOD.AWQ or METHOD.GPTQ as needed
-        kernel=KERNEL.GEMV,        # FORMAT.GEMM / FORMAT.GEMV also available
+        kernel=KERNEL.MARLIN,        # KERNEL.Marlin / KERNEL.GEMM / KERNEL.GEMV also available
     ) 
 
     logger.info("Loading pretrained model for quantization...")
